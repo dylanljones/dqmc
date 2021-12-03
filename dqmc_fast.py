915b339b2c4905a974c8ed379a429a0b18214ee8
dylanljones <dylanljones94@gmail.com>
2021-12-03 11:43:29 +0100

    updated scripts

--- dqmc_fast.py
+++ dqmc_fast.py
@@ -7,74 +7,11 @@
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
-from dqmc.dqmc import init_qmc, compute_greens, compute_timestep_mats, iteration_fast
-from dqmc.model import hubbard_hypercube
-from dqmc import mfuncs
 from tqdm import tqdm
+from dqmc import DQMC, hubbard_hypercube, mfuncs
 
 logger = logging.getLogger("dqmc")
 logger.setLevel(logging.WARNING)
-
-
-class DQMC:
-
-    def __init__(self, model, num_timesteps, time_dir=+1, bmat_dir=None):
-        # Init QMC variables
-        self.exp_k, self.nu, self.config = init_qmc(model, num_timesteps)
-
-        # Set up time direction and order of inner loops
-        if bmat_dir is None:
-            # Default B-matrix direction is the reverse of the time direction
-            bmat_dir = - time_dir
-        self.bmat_order = np.arange(self.config.shape[1], dtype=np.int64)[::bmat_dir]
-        self.times = np.arange(self.config.shape[1], dtype=np.int64)[::time_dir]
-        self.sites = np.arange(self.config.shape[0], dtype=np.int64)
-
-        # Pre-compute time flow matrices
-        self.bmats_up, self.bmats_dn = compute_timestep_mats(self.exp_k, self.nu, self.config)
-
-        # Initialize QMC statistics
-        self.status = ""
-        self.acceptance_probs = list()
-
-        # Initialization
-        self._gf_up, self._gf_dn = self.greens()
-
-    def recompute_greens(self):
-        self._gf_up, self._gf_dn = self.greens()
-
-    def greens(self):
-        return compute_greens(self.bmats_up, self.bmats_dn, self.bmat_order)
-
-    def get_greens(self):
-        return self._gf_up, self._gf_dn
-
-    def iteration(self):
-        accepted = iteration_fast(self.exp_k, self.nu, self.config, self.bmats_up, self.bmats_dn,
-                                  self._gf_up, self._gf_dn, self.times)
-        # Compute and save acceptance ratio
-        acc_ratio = accepted / self.config.size
-        self.acceptance_probs.append(acc_ratio)
-        # Recompute Green's functions
-        self.recompute_greens()
-
-    def simulate(self, warmup, measure, callback):
-        sweeps = warmup + measure
-        out = 0.
-        # Run sweeps
-        self.status = "warmup"
-        for sweep in range(sweeps):
-            self.iteration()
-            logger.info("[%s] %3d Ratio: %.2f", self.status, sweep, self.acceptance_probs[-1])
-            # perform measurements
-            if sweep > warmup:
-                self.status = "measurements"
-                gf_up, gf_dn = self.get_greens()
-                if callback is not None:
-                    out += callback(gf_up, gf_dn)
-                else:
-                    out += np.array([gf_up, gf_dn])
-        return out / measure
 
 
 def build_temperature_array(temps, model, num_timesteps, warmup=500, measure=3000, callback=None):
@@ -88,12 +25,12 @@ def build_temperature_array(temps, model, num_timesteps, warmup=500, measure=300
 
 
 def main():
-    shape = 10
+    shape = (4, 4)
     u, mu, hop = 4.0, 0.0, 1.0
-    temps = np.geomspace(0.1, 100, 50)
-    num_timesteps = 100
+    temps = np.geomspace(0.2, 100, 50)
+    num_timesteps = 50
     warmup, measure = 500, 2000
-    model = hubbard_hypercube(shape, u=u, mu=mu, hop=hop, beta=1.0, periodic=(0,))
+    model = hubbard_hypercube(shape, u=u, mu=mu, hop=hop, beta=1.0, periodic=(0,1))
 
     moments = build_temperature_array(temps, model, num_timesteps, warmup, measure,
                                       callback=mfuncs.mz_moment)
--- main.py
+++ main.py
@@ -7,96 +7,10 @@
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
-from dqmc import HubbardModel, BaseDQMC, iteration_fast, mfuncs, compute_m_matrices, iteration_det
-from tqdm import tqdm
+from dqmc import DQMC, hubbard_hypercube, mfuncs
 
 logger = logging.getLogger("dqmc")
-logger.setLevel(logging.WARNING)
-
-
-class DetDQMC(BaseDQMC):
-
-    def __init__(self, model, num_timesteps, time_dir=+1):
-        self._old_det = 0
-        super().__init__(model, num_timesteps, time_dir)
-
-    def initialize(self):
-        m_up, m_dn = compute_m_matrices(self.bmats_up, self.bmats_dn, self.times)
-        det_up = np.linalg.det(m_up)
-        det_dn = np.linalg.det(m_dn)
-        self._old_det = det_up * det_dn
-
-    def iteration(self):
-        old_det, accepted = iteration_det(self.exp_k, self.nu, self.config,
-                                          self.bmats_up, self.bmats_dn, self._old_det, self.times)
-        self._old_det = old_det
-        # Compute and save acceptance ratio
-        acc_ratio = accepted / self.config.size
-        self.acceptance_probs.append(acc_ratio)
-
-
-class FastDQMC(BaseDQMC):
-
-    def __init__(self, model, num_timesteps):
-        self.gf_up = np.empty([])
-        self.gf_dn = np.empty([])
-        super().__init__(model, num_timesteps, time_dir=+1)
-
-    def initialize(self):
-        self.gf_up, self.gf_dn = self.greens()
-
-    def iteration(self):
-        accepted = iteration_fast(self.exp_k, self.nu, self.config, self.bmats_up, self.bmats_dn,
-                                  self.gf_up, self.gf_dn, self.times)
-        # Compute and save acceptance ratio
-        acc_ratio = accepted / self.config.size
-        self.acceptance_probs.append(acc_ratio)
-
-    def get_greens(self):
-        return self.gf_up, self.gf_dn
-
-
-class DQMC(BaseDQMC):
-
-    def __init__(self, model, num_timesteps, mode="fast"):
-        self._gf_up = np.empty([])
-        self._gf_dn = np.empty([])
-        self._old_det = 0.
-        self.mode = mode
-        self._iter = self.iteration_fast if mode == "fast" else self.iteration_det
-        super().__init__(model, num_timesteps, time_dir=+1)
-
-    def initialize(self):
-        m_up, m_dn = compute_m_matrices(self.bmats_up, self.bmats_dn, self.times)
-        det_up = np.linalg.det(m_up)
-        det_dn = np.linalg.det(m_dn)
-        self._old_det = det_up * det_dn
-        self._gf_up = np.linalg.inv(m_up)
-        self._gf_dn = np.linalg.inv(m_dn)
-
-    def iteration_fast(self):
-        accepted = iteration_fast(self.exp_k, self.nu, self.config, self.bmats_up, self.bmats_dn,
-                                  self._gf_up, self._gf_dn, self.times)
-
-        return accepted
-
-    def iteration_det(self):
-        old_det, accepted = iteration_det(self.exp_k, self.nu, self.config,
-                                          self.bmats_up, self.bmats_dn, self._old_det, self.times)
-        self._old_det = old_det
-        return accepted
-
-    def iteration(self):
-        accepted = self._iter()
-        # Compute and save acceptance ratio
-        acc_ratio = accepted / self.config.size
-        self.acceptance_probs.append(acc_ratio)
-
-    def get_greens(self):
-        if self.mode == "fast":
-            return self._gf_up, self._gf_dn
-        else:
-            return self.greens()
+logger.setLevel(logging.INFO)
 
 
 def plot_acceptance_probs(probs, warmup):
@@ -110,27 +24,28 @@ def plot_acceptance_probs(probs, warmup):
     ax.axvline(x=warmup, color="r", ls="--")
 
 
+def run_dqmc(num_sites, num_timesteps, u, temp, mu=0., hop=1., warmup=500, measure=2000,
+             callback=None):
+    model = hubbard_hypercube(num_sites, u=u, mu=mu, hop=hop, beta=1 / temp, periodic=True)
+    dqmc = DQMC(model, num_timesteps)
+    return dqmc.simulate(warmup, measure, callback)
+
+
+def pformat_result(value, error, dec=5):
+    return f"{value:.{dec}f}±{error:.{dec}f}"
+
+
 def main():
-    u, mu, hop = 4.0, 0.0, 1.0
     num_sites = 10
     num_timesteps = 50
     warmup = 500
     measure = 3000
+    u, mu, hop = 4.0, 0.0, 1.0
+    temp = 1
 
-    temps = np.geomspace(0.2, 100, 50)
-    moments = np.zeros((len(temps), 10))
-    for i in tqdm(range(len(temps))):
-        model = HubbardModel(num_sites, u=u, mu=mu, hop=hop, beta=1 / temps[i])
-        dqmc = FastDQMC(model, num_timesteps)
-        mom = dqmc.simulate(warmup, measure, callback=mfuncs.mz_moment)
-        moments[i] = mom
-
-    fig, ax = plt.subplots()
-    ax.set_xscale("log")
-    ax.plot(temps, moments.T[4])
-    ax.set_xlabel("T")
-    ax.set_ylabel(r"$\langle m_z^2 \rangle$")
-    plt.show()
+    res = run_dqmc(num_sites, num_timesteps, u, temp, mu, hop, warmup, measure, mfuncs.mz_moment)
+    print(res)
+    print(pformat_result(np.mean(res), np.std(res), dec=4))
 
 
 if __name__ == "__main__":
