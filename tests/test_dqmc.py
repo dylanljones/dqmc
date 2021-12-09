3191659290351fd8a07e12bf9b5ecad037dc86d2
dylanljones <dylanljones94@gmail.com>
2021-12-09 15:54:45 +0100

    added unittests

--- tests/test_dqmc.py
+++ tests/test_dqmc.py
@@ -13,8 +13,29 @@ from hypothesis import given, strategies as st
 from dqmc import dqmc, hubbard_hypercube
 
 
-def test_init_qmc():
-    pass
+def test_init_qmc_atomic():
+    model = hubbard_hypercube(5, u=1, hop=0., beta=1.0)
+    exp_k, nu, config = dqmc.init_qmc(model, 100)
+    assert_array_equal(np.eye(model.num_sites), exp_k)
+    assert abs(nu - 0.1) < 1e-3
+
+
+def test_init_qmc_noninter():
+    model = hubbard_hypercube(5, u=0, hop=1., beta=1.0)
+    exp_k, nu, config = dqmc.init_qmc(model, 100)
+
+    expected = np.eye(model.num_sites)
+    np.fill_diagonal(expected[1:, :], -0.01)
+    np.fill_diagonal(expected[:, 1:], -0.01)
+    assert_array_almost_equal(expected, exp_k, decimal=3)
+    assert nu == 0
+
+
+def test_init_qmc_zerot():
+    model = hubbard_hypercube(5, u=1, hop=1., beta=0.0)
+    exp_k, nu, config = dqmc.init_qmc(model, 100)
+    assert_array_equal(np.eye(model.num_sites), exp_k)
+    assert nu == 0
 
 
 @given(st.floats(0.1, 5), st.floats(0, 5), st.floats(0.1, 10))
@@ -92,31 +113,63 @@ def test_compute_timeflow_map(u, mu, beta):
     assert_array_equal(expected, result)
 
 
-def test_compute_greens():
-    pass
+@given(st.integers(1, 5), st.integers(1, 5), st.integers(0, 9), st.integers(0, 99))
+def test_update(u, beta, i, t):
+    num_sites = 10
+    num_timesteps = 100
+    eps, mu, hop = 0.0, 0.0, 1.0
 
+    model = hubbard_hypercube(num_sites, u, eps, hop, mu, beta, periodic=True)
+    exp_k, nu, config = dqmc.init_qmc(model, num_timesteps)
+    bmats_up, bmats_dn = dqmc.compute_timestep_mats(exp_k, nu, config)
 
-def test_update():
-    pass
+    old_spin = config[i, t]
+    dqmc.update(exp_k, nu, config, bmats_up, bmats_dn, i, t)
 
+    bmats_up_new, bmats_dn_new = dqmc.compute_timestep_mats(exp_k, nu, config)
 
-def test_iteration_det():
-    pass
+    assert config[i, t] == -old_spin
+    assert_array_equal(bmats_up_new, bmats_up)
+    assert_array_equal(bmats_dn_new, bmats_dn)
 
 
 def test_compute_acceptance_fast():
     pass
 
 
-def test_update_greens():
+def test_compute_greens():
     pass
 
 
-def test_update_greens2():
-    pass
+@given(st.integers(1, 5), st.integers(1, 5), st.integers(0, 9))
+def test_update_greens(u, beta, i):
+    num_sites = 10
+    num_timesteps = 100
+    eps, mu, hop = 0.0, 0.0, 1.0
+    t = 0  # Only use first time slice to prevent need for wrapping
+
+    model = hubbard_hypercube(num_sites, u, eps, hop, mu, beta, periodic=True)
+    exp_k, nu, config = dqmc.init_qmc(model, num_timesteps)
+    bmats_up, bmats_dn = dqmc.compute_timestep_mats(exp_k, nu, config)
+
+    bmat_order = np.arange(config.shape[1], dtype=np.int64)[::-1]
+    gf_up, gf_dn = dqmc.compute_greens(bmats_up, bmats_dn, bmat_order)
+
+    # -----------Update Green's function --------------
+    dqmc.update_greens(nu, config, gf_up, gf_dn, i, t)
+    dqmc.update(exp_k, nu, config, bmats_up, bmats_dn, i, t)
+    gf_up_ref, gf_dn_ref = dqmc.compute_greens(bmats_up, bmats_dn, bmat_order)
+    # -------------------------------------------------
+
+    assert_array_almost_equal(gf_up, gf_up_ref, decimal=5)
+    assert_array_almost_equal(gf_dn, gf_dn_ref, decimal=5)
 
 
 def test_wrap_greens():
+    pass
+
+
+def test_iteration_det():
     pass
 
 
