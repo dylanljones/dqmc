b0a792e2eef69d279222a7399417223162f7bc78
dylanljones <dylanljones94@gmail.com>
2021-12-13 15:39:33 +0100

    added additional tests (only `compute_greens` still missing)

--- tests/test_dqmc.py
+++ tests/test_dqmc.py
@@ -3,14 +3,20 @@
 # This code is part of dqmc.
 #
 # Copyright (c) 2021, Dylan Jones
+"""Tests for the main dqmc methods.
 
-import random
+The deadline of the hypothesis tests have been increased to prevent `flaky` errors.
+"""
+
 import numpy as np
 import scipy.linalg as la
 from functools import reduce
 from numpy.testing import assert_array_equal, assert_array_almost_equal
-from hypothesis import given, strategies as st
+from hypothesis import given, settings, assume, strategies as st
 from dqmc import dqmc, hubbard_hypercube
+
+settings.register_profile("dqmc", deadline=20000, max_examples=100)
+settings.load_profile("dqmc")
 
 
 def test_init_qmc_atomic():
@@ -38,12 +44,15 @@ def test_init_qmc_zerot():
     assert nu == 0
 
 
-@given(st.floats(0.1, 5), st.floats(0, 5), st.floats(0.1, 10))
-def test_compute_timestep_mat(u, mu, beta):
-    model = hubbard_hypercube(5, u, mu=mu, beta=beta)
-    exp_k, nu, config = dqmc.init_qmc(model, 200)
+@given(st.floats(0.1, 5), st.floats(0, 5), st.floats(0.1, 10), st.integers(0, 199))
+def test_compute_timestep_mat(u, mu, beta, t):
+    num_sites = 10
+    num_timesteps = 200
+    eps, hop = 0.0, 1.0
+    assume(u * hop * (beta / num_timesteps)**2 < 0.1)
 
-    t = random.randint(0, 199)
+    model = hubbard_hypercube(num_sites, u, eps, hop, mu, beta, periodic=True)
+    exp_k, nu, config = dqmc.init_qmc(model, num_timesteps)
 
     # Spin up
     sigma = +1
@@ -62,29 +71,38 @@ def test_compute_timestep_mat(u, mu, beta):
 
 @given(st.floats(0.1, 5), st.floats(0, 5), st.floats(0.1, 10))
 def test_compute_timestep_mats(u, mu, beta):
-    model = hubbard_hypercube(5, u, mu=mu, beta=beta)
-    exp_k, nu, config = dqmc.init_qmc(model, 200)
+    num_sites = 10
+    num_timesteps = 200
+    eps, hop = 0.0, 1.0
+    assume(u * hop * (beta / num_timesteps)**2 < 0.1)
+
+    model = hubbard_hypercube(num_sites, u, eps, hop, mu, beta, periodic=True)
+    exp_k, nu, config = dqmc.init_qmc(model, num_timesteps)
     bmats_up, bmats_dn = dqmc.compute_timestep_mats(exp_k, nu, config)
 
     # Check order of B-matrices
+    t1 = num_timesteps - 1
     first_up = dqmc.compute_timestep_mat(exp_k, nu, config, t=0, sigma=+1)
     first_dn = dqmc.compute_timestep_mat(exp_k, nu, config, t=0, sigma=-1)
-    last_up = dqmc.compute_timestep_mat(exp_k, nu, config, t=199, sigma=+1)
-    last_dn = dqmc.compute_timestep_mat(exp_k, nu, config, t=199, sigma=-1)
+    last_up = dqmc.compute_timestep_mat(exp_k, nu, config, t=t1, sigma=+1)
+    last_dn = dqmc.compute_timestep_mat(exp_k, nu, config, t=t1, sigma=-1)
     assert_array_equal(last_up, bmats_up[-1])
     assert_array_equal(last_dn, bmats_dn[-1])
     assert_array_equal(first_up, bmats_up[0])
     assert_array_equal(first_dn, bmats_dn[0])
 
 
-@given(st.floats(0.1, 5), st.floats(0, 5), st.floats(0.1, 10))
-def test_update_timestep_mats(u, mu, beta):
-    model = hubbard_hypercube(5, u, mu=mu, beta=beta)
-    exp_k, nu, config = dqmc.init_qmc(model, 200)
-    bmats_up, bmats_dn = dqmc.compute_timestep_mats(exp_k, nu, config)
+@given(st.floats(0.1, 5), st.floats(0, 5), st.floats(0.1, 10),
+       st.integers(0, 9), st.integers(0, 99))
+def test_update_timestep_mats(u, mu, beta, i, t):
+    num_sites = 10
+    num_timesteps = 200
+    eps, hop = 0.0, 1.0
+    assume(u * hop * (beta / num_timesteps)**2 < 0.1)
 
-    t = random.randint(0, 199)
-    i = random.randint(0, 4)
+    model = hubbard_hypercube(num_sites, u, eps, hop, mu, beta, periodic=True)
+    exp_k, nu, config = dqmc.init_qmc(model, num_timesteps)
+    bmats_up, bmats_dn = dqmc.compute_timestep_mats(exp_k, nu, config)
 
     config[i, t] = -config[i, t]
 
@@ -98,9 +116,15 @@ def test_update_timestep_mats(u, mu, beta):
 
 @given(st.floats(0.1, 5), st.floats(0, 5), st.floats(0.1, 10))
 def test_compute_timeflow_map(u, mu, beta):
-    model = hubbard_hypercube(5, u, mu=mu, beta=beta)
-    exp_k, nu, config = dqmc.init_qmc(model, 200)
+    num_sites = 10
+    num_timesteps = 200
+    eps, hop = 0.0, 1.0
+    assume(u * hop * (beta / num_timesteps)**2 < 0.1)
+
+    model = hubbard_hypercube(num_sites, u, eps, hop, mu, beta, periodic=True)
+    exp_k, nu, config = dqmc.init_qmc(model, num_timesteps)
     bmats_up, bmats_dn = dqmc.compute_timestep_mats(exp_k, nu, config)
+
     order = np.arange(config.shape[1]).astype(np.int64)
 
     # Spin up
@@ -118,6 +142,7 @@ def test_update(u, beta, i, t):
     num_sites = 10
     num_timesteps = 100
     eps, mu, hop = 0.0, 0.0, 1.0
+    assume(u * hop * (beta / num_timesteps)**2 < 0.1)
 
     model = hubbard_hypercube(num_sites, u, eps, hop, mu, beta, periodic=True)
     exp_k, nu, config = dqmc.init_qmc(model, num_timesteps)
@@ -133,8 +158,41 @@ def test_update(u, beta, i, t):
     assert_array_equal(bmats_dn_new, bmats_dn)
 
 
-def test_compute_acceptance_fast():
-    pass
+@given(st.integers(1, 10), st.integers(1, 5), st.integers(0, 9), st.integers(0, 99))
+def test_compute_acceptance_fast(u, beta, i, t):
+    num_sites = 10
+    num_timesteps = 100
+    eps, mu, hop = 0.0, 0.0, 1.0
+    assume(u * hop * (beta / num_timesteps)**2 < 0.1)
+
+    model = hubbard_hypercube(num_sites, u, eps, hop, mu, beta, periodic=True)
+    exp_k, nu, config = dqmc.init_qmc(model, num_timesteps)
+    bmats_up, bmats_dn = dqmc.compute_timestep_mats(exp_k, nu, config)
+
+    order = np.arange(num_timesteps)[::-1]
+    order = np.roll(order, t)
+    gf_up, gf_dn = dqmc.compute_greens(bmats_up, bmats_dn, order)
+
+    # Compute fast acceptance *before* spin flip
+    d = dqmc.compute_acceptance_fast(nu, config, gf_up, gf_dn, i, t)
+
+    # Compute old determinants before flip
+    m_up, m_dn = dqmc.compute_m_matrices(bmats_up, bmats_dn, order)
+    det_up = la.det(m_up)
+    det_dn = la.det(m_dn)
+    old_det = det_up * det_dn
+
+    # Flip spin
+    dqmc.update(exp_k, nu, config, bmats_up, bmats_dn, i, t)
+
+    # Compute acceptance ratio
+    m_up, m_dn = dqmc.compute_m_matrices(bmats_up, bmats_dn, order)
+    det_up = la.det(m_up)
+    det_dn = la.det(m_dn)
+    new_det = det_up * det_dn
+    d_slow = min(abs(new_det / old_det), 1.0)
+
+    assert abs(d_slow - d) < 1e6
 
 
 def test_compute_greens():
@@ -146,6 +204,8 @@ def test_update_greens(u, beta, i):
     num_sites = 10
     num_timesteps = 100
     eps, mu, hop = 0.0, 0.0, 1.0
+    assume(u * hop * (beta / num_timesteps)**2 < 0.1)
+
     t = 0  # Only use first time slice to prevent need for wrapping
 
     model = hubbard_hypercube(num_sites, u, eps, hop, mu, beta, periodic=True)
@@ -165,13 +225,28 @@ def test_update_greens(u, beta, i):
     assert_array_almost_equal(gf_dn, gf_dn_ref, decimal=5)
 
 
-def test_wrap_greens():
-    pass
+@given(st.integers(1, 5), st.integers(1, 5), st.integers(0, 99))
+def test_wrap_greens(u, beta, t):
+    num_sites = 10
+    num_timesteps = 100
+    eps, mu, hop = 0.0, 0.0, 1.0
+    assume(u * hop * (beta / num_timesteps)**2 < 0.1)
 
+    model = hubbard_hypercube(num_sites, u, eps, hop, mu, beta, periodic=True)
+    exp_k, nu, config = dqmc.init_qmc(model, num_timesteps)
+    bmats_up, bmats_dn = dqmc.compute_timestep_mats(exp_k, nu, config)
 
-def test_iteration_det():
-    pass
+    # Compute Green's function of time slice `t`
+    order = np.arange(num_timesteps)[::-1]
+    order = np.roll(order, t)
+    gf_up, gf_dn = dqmc.compute_greens(bmats_up, bmats_dn, order)
 
+    # Wrap Greens function to next time slice `t+1`
+    dqmc.wrap_greens(bmats_up, bmats_dn, gf_up, gf_dn, t)
 
-def test_iteration_fast():
-    pass
+    # Re-compute Green's function for next time slice `t+1`
+    order = np.roll(order, +1)
+    gf_up_ref, gf_dn_ref = dqmc.compute_greens(bmats_up, bmats_dn, order)
+
+    assert_array_almost_equal(gf_up_ref, gf_up, decimal=5)
+    assert_array_almost_equal(gf_dn_ref, gf_dn, decimal=5)
