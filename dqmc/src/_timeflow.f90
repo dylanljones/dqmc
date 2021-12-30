6c8ff879a558d8c2c777fa882a569562d8a5eb4c
dylanljones <dylanljones94@gmail.com>
2021-12-30 21:34:03 +0100

    remove internal subroutines

--- dqmc/src/_timeflow.f90
+++ dqmc/src/_timeflow.f90
@@ -1,204 +1,6 @@
 ! Created by Dylan Jones on 30/12/2021.
 
 
-subroutine qrp(N, A, JPVT, TAU, LWORK, INFO)
-
-    implicit none
-
-    integer :: N
-    double precision, dimension(*) :: A(N, N)
-    double precision, dimension(*) :: TAU(N)
-    integer, dimension(*) :: JPVT(N)
-    integer, intent(inout) :: LWORK, INFO
-    double precision, dimension(:), allocatable :: WORK
-
-    JPVT(:) = 0  ! mark all columns as free
-    ! Perform optimal LWORK query by calling DGEQP3 with LWORK=-1
-    LWORK = -1
-    allocate(WORK(1))
-    call dgeqp3(N, N, A, N, JPVT, TAU, WORK, LWORK, INFO)
-    ! Perform QR decomposition with optimal LWORK
-    LWORK = INT(WORK(1))
-    deallocate(WORK)
-    allocate(WORK(LWORK))
-    call dgeqp3(N, N, A, N, JPVT, TAU, WORK, LWORK, INFO)
-
-end subroutine qrp
-
-
-
-subroutine extract_diag(N, A, D)
-
-    implicit none
-
-    integer :: N
-    double precision, dimension(*) :: A(N, N)
-    double precision, dimension(*) :: D(N)
-    integer :: i
-
-    do i=1, N
-        D(i) = A(i, i)
-        if (D(i) == 0.d0) D(i) = 1.d0
-    end do
-
-end subroutine extract_diag
-
-
-
-subroutine build_t1(N, Q, JPVT, D, T)
-
-    implicit none
-
-    integer :: N
-    double precision, dimension(*) :: Q(N, N)
-    double precision, dimension(*) :: D(N)
-    double precision, dimension(*) :: T(N, N)
-    integer, dimension(*) :: JPVT(N)
-    integer :: i, j
-
-    ! Construct matrix T_1 and store in T
-    T(:, :) = 0.d0
-    do j=1, N
-        do i=1, j
-            T(i, JPVT(j)) = (1 / D(i)) * Q(i, j)
-        end do
-    end do
-
-end subroutine build_t1
-
-
-
-subroutine build_w(N, Q, D, W, TAU, LWORK, INFO)
-
-    implicit none
-
-    integer :: N
-    double precision, dimension(*) :: Q(N, N)
-    double precision, dimension(*) :: D(N)
-    double precision, dimension(*) :: W(N, N)
-    double precision, dimension(*) :: TAU(N)
-    integer :: LWORK, INFO
-    double precision, dimension(*) :: WORK(LWORK)
-    integer :: i
-
-    ! Multiply with Q from the right, overwriting the 'W' matrix
-    call dormqr("R", "N", N, N, N, Q, N, TAU, W, N, WORK, LWORK, INFO)
-    ! Scale by previous diagonal entries
-    do i=1, N
-        call dscal(N, D(i), W(:, i), 1)
-    end do
-
-end subroutine build_w
-
-
-
-subroutine pre_pivot(N, W, JPVT)
-
-    implicit none
-
-    integer :: N
-    double precision, dimension(*) :: W(N, N)
-    integer, dimension(*) :: JPVT(N)
-    double precision, dimension(*) :: NORM(N)
-    double precision :: dnrm2
-    integer :: i, j
-
-    ! Compute column norms
-    do j=1, N
-        NORM(j) = dnrm2(N, W(:, j), 1)
-    end do
-    ! Determine jpvt with an insertion sort
-    JPVT(1) = 1
-    do i = 2, N
-        j = i - 1
-        do while (j >= 1)
-            if (NORM(JPVT(j)) >= NORM(i)) exit
-            JPVT(j + 1) = JPVT(j)
-            j = j - 1
-        end do
-        JPVT(j + 1) = i
-    end do
-
-end subroutine pre_pivot
-
-
-
-subroutine pre_pivot_qrf(N, W, Q, JPVT, TAU, LWORK, INFO)
-
-    implicit none
-
-    integer :: N
-    double precision, dimension(*) :: W(N, N)
-    double precision, dimension(*) :: Q(N, N)
-    integer, dimension(*) :: JPVT(N)
-    double precision, dimension(*) :: TAU(N)
-    integer :: LWORK, INFO
-    double precision, dimension(*) :: WORK(LWORK)
-    double precision, dimension(*) :: NORM(N)
-    double precision :: dnrm2
-    integer :: i, j
-
-    ! Compute column norms
-    do j=1, N
-        NORM(j) = dnrm2(N, W(:, j), 1)
-    end do
-
-    ! Determine jpvt with an insertion sort
-    JPVT(1) = 1
-    do i = 2, N
-        j = i - 1
-        do while (j >= 1)
-            if (NORM(JPVT(j)) >= NORM(i)) exit
-            JPVT(j + 1) = JPVT(j)
-            j = j - 1
-        end do
-        JPVT(j + 1) = i
-    end do
-
-    ! store pre-pivoted W in Q
-    do j=1, N
-        Q(:, j) = W(:, JPVT(j))
-    end do
-    ! Perform QR decomposition on Q
-    call dgeqrf(N, N, Q, N, TAU, WORK, LWORK, INFO)
-
-end subroutine pre_pivot_qrf
-
-
-
-subroutine build_t(N, Q, JPVT, D, T)
-
-    implicit none
-
-    integer :: N
-    double precision, dimension(*) :: Q(N, N)
-    double precision, dimension(*) :: D(N)
-    double precision, dimension(*) :: T(N, N)
-    integer, dimension(*) :: JPVT(N)
-    double precision, dimension(*) :: V(N, N), QD(N, N)
-    integer :: i, j
-
-    ! Multiply inverse diagonal matrix with the upper triangular R matrix
-    QD(:, :) = 0.d0
-    do j=1, N
-        do i=1, j
-            QD(i, j) = (1 / D(i)) * Q(i, j)
-        end do
-    end do
-
-    ! Apply pivoting permutation to the rows of previous T
-    do i=1, N
-        V(i, :) = T(JPVT(i), :)
-    end do
-    T(:, :) = V(:, :)
-
-    ! multiply with upper triangular matrix, overwriting 'T' in-place;
-    call dtrmm("L", "U", "N", "N", N, N, 1.0d0, QD, N, T, N)
-
-end subroutine build_t
-
-
-
 subroutine timeflow_map(L, N, TSM, Q, D, T, TAU, LWORK, INFO)
 
     implicit none
@@ -237,7 +39,10 @@ subroutine timeflow_map(L, N, TSM, Q, D, T, TAU, LWORK, INFO)
     ! ---------------------------
 
     ! Extract diagonal elements of R (upper triangular matrix of Q)
-    call extract_diag(N, Q, D)
+    do i=1, N
+        D(i) = Q(i, i)
+        if (D(i) == 0.d0) D(i) = 1.d0
+    end do
     ! Construct matrix T_1 and store in T
     T(:, :) = 0.d0
     do j=1, N
@@ -293,7 +98,10 @@ subroutine timeflow_map(L, N, TSM, Q, D, T, TAU, LWORK, INFO)
         ! ---------------------------
 
         ! Extract diagonal elements of R (upper triangular matrix of Q)
-        call extract_diag(N, Q, D)
+        do i=1, N
+            D(i) = Q(i, i)
+            if (D(i) == 0.d0) D(i) = 1.d0
+        end do
         ! Multiply inverse diagonal matrix with the upper triangular R matrix
         W(:, :) = 0.d0
         do j=1, N
