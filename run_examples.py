a650ae52485531c633bb3fe61992bd4758783b99
dylanljones <dylanljones94@gmail.com>
2022-01-10 20:49:47 +0100

    update year

--- run_examples.py
+++ run_examples.py
@@ -3,6 +3,10 @@
 # This code is part of dqmc.
 #
 # Copyright (c) 2022, Dylan Jones
+#
+# This code is licensed under the MIT License. The copyright notice in the
+# LICENSE file in the root directory and this permission notice shall
+# be included in all copies or substantial portions of the Software.
 
 import os.path
 import numpy as np
--- tests/test_dqmc.py
+++ tests/test_dqmc.py
@@ -2,7 +2,11 @@
 #
 # This code is part of dqmc.
 #
-# Copyright (c) 2021, Dylan Jones
+# Copyright (c) 2022, Dylan Jones
+#
+# This code is licensed under the MIT License. The copyright notice in the
+# LICENSE file in the root directory and this permission notice shall
+# be included in all copies or substantial portions of the Software.
 
 """Tests for the main dqmc methods.
 
