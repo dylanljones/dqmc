f213c6897694faca0b0f7bd7ab86f451230a238c
dylanljones <dylanljones94@gmail.com>
2022-02-02 16:46:13 +0100

    remove Python 3.10 from supported versions

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering :: Physics',
     ],
     keywords=["quantum monte carlo", "physics", "dqmc", ""],
